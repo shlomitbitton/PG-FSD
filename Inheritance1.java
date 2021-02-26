15ee2d23b651bc5742c754196375edb93e045810
Shlomit <shlomitgazit12@hotmail.com>
2021-02-25 16:07:22 -0800

    ,

---
+++ Inheritance1.java
@@ -0,0 +1,9 @@
+public class Inheritance1{
+
+  
+  public static String A="I am string A from class Inheritance1";
+
+  public void methodFromInheritance(){
+    System.out.println("I am methodFromInheritance 1");
+  }
+}
